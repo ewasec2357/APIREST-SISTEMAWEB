abc47cca20dd517b32fd3428fd742a64619f183e
Edward Soto <edward23572357@gmail.com>
2023-02-14 18:31:43 -0500

    17vo commit

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -127,9 +127,11 @@ const actualizarUsuario = async (req, res = response) => {
         campos.dni = dni;
         campos.email = email;
 
+        const usuario = new Usuario( req.body );
+        
         const salt = bcrypt.genSaltSync();
         usuario.password = bcrypt.hashSync( password, salt );
-        
+
         const usuarioActualizado = await Usuario.findByIdAndUpdate( uid, campos, { new: true } );
 
         res.json({
